f8a1284d580cd0345b7395e26f4ea795259f6bc3
longdo <long.do19@gmail.com>
2015-05-20 16:13:58 +0200

    Code Maxime

---
+++ test/test_composant2/test_composant2/Composant2.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <string>
+using std::string;
+
+class Composant2{
+public:
+	Composant2();//Constructeur
+	double doMonteCarlo(string typePayOff, double maturity, double strike, int nbIterations);
+	double* getPath();
+	double pricePath(string typePayOff, double* vecteur, double strike, double maturity);
+
+private:
+	
+
+
+};
